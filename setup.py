6c5adcdefeb93777cddf2b737f48d0388b0914d7
MCgamer <kidsink101@gmail.com>
2020-11-16 12:07:31 -0600

    Added Setup.py for building exe

---
+++ setup.py
@@ -0,0 +1,9 @@
+import cx_Freeze
+
+executables = [cx_Freeze.Executable("abitracey.py")]
+
+cx_Freeze.setup(
+    name="A bit Racey",
+    options={"build_exe": {"packages":["pygame"],
+                           "include_files":["car.png", "highscore"]}},
+    executables = executables)
